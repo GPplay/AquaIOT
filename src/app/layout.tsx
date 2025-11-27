72bb908c2c9e5b609ff3061ad4acb1ed44ce4e31
Gybram LLAMAS <gybramllamasp@gmail.com>
2025-11-27 01:21:16 +0000

    cambia el fab-icon de la pagina que aun tiene el logo de firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: 'AquaGuard',
   description: 'Sistema de Alerta Temprana de Inundaciones',
   icons: {
-    icon: '/logo.png',
+    icon: '/logo.png?v=1',
   },
 };
 
