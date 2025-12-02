4d7bfbca012c48f04146f896c554697946072520
Gybram LLAMAS <gybramllamasp@gmail.com>
2025-12-02 00:15:06 +0000

    comenta esto "throw new Error('no se encontro servidor');" y esto "throw

--- src/services/api.ts
+++ src/services/api.ts
@@ -22,7 +22,7 @@ export async function login(email: string, password: string): Promise<{ token: s
   } catch (error) {
     // This will catch network errors (e.g., server is down)
     console.error('no se encontro base de datos');
-    throw new Error('no se encontro servidor');
+    // throw new Error('no se encontro servidor');
   }
 
 
